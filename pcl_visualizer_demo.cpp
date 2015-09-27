b6536d4f45087434c8acd8a3288389c6f3a9fbb9
Victor Arribas <v.arribas.urjc@gmail.com>
2015-09-27 16:55:43 +0200

    viewport playing

--- pcl_visualizer_demo.cpp
+++ pcl_visualizer_demo.cpp
@@ -150,24 +150,35 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> viewportsVis (
 
   int v1(0);
   viewer->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
+  viewer->setCameraPosition(1,0,0, 0,1,0, v1);
   viewer->setBackgroundColor (0, 0, 0, v1);
   viewer->addText("Radius: 0.01", 10, 10, "v1 text", v1);
   pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
   viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud1", v1);
-
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
+  viewer->addCoordinateSystem(1.0, "reference", v1);
+/*
   int v2(0);
   viewer->createViewPort(0.5, 0.0, 1.0, 1.0, v2);
+#if 1
+  viewer->createViewPortCamera(v2);
+  viewer->setCameraPosition(0,0, 10, 0,1,0, v2);
+#endif
   viewer->setBackgroundColor (0.3, 0.3, 0.3, v2);
   viewer->addText("Radius: 0.1", 10, 10, "v2 text", v2);
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> single_color(cloud, 0, 255, 0);
   viewer->addPointCloud<pcl::PointXYZRGB> (cloud, single_color, "sample cloud2", v2);
-
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud2");
-  viewer->addCoordinateSystem (1.0);
+  viewer->addCoordinateSystem (1.0, "reference", v2);
 
   viewer->addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal> (cloud, normals1, 10, 0.05, "normals1", v1);
   viewer->addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal> (cloud, normals2, 10, 0.05, "normals2", v2);
+*/
+
+  int v3(0);
+  viewer->createViewPort(0.9, 0.8, 1.0, 1.0, v3);
+  viewer->setBackgroundColor(1.0, 1.0, 1.0, v3);
+  viewer->addCoordinateSystem(1.0, "reference", v3);
 
   return (viewer);
 }
